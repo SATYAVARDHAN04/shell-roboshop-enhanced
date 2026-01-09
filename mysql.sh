7752b28a7fc993fcc409181cbf762755048a36a3
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-01-09 20:26:01 +0530

    roboshop

--- mysql.sh
+++ mysql.sh
@@ -10,26 +10,13 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p ${LOGS_FOLDER}
+app_name=mysql
 
-if [ $USERID -ne 0 ]
-then 
-	echo -e "${red}Error:${reset} Please run with root access...." | tee -a $LOG_FILE
-	exit 1
-else
-	echo -e "${green}Running with root access!!! ${reset}" | tee -a $LOG_FILE
-fi
+source ./common.sh
+check_root
+Validate
 
 read -s -p "Enter the mysql root password: " mysqlpasswd
-
-Validate() {
-	if [ $1 -eq 0 ]
-	then 
-		echo -e "${green}$2 is done successfully${reset}" | tee -a $LOG_FILE
-	else
-		echo -e "${red}Error: $2 failed${reset}" | tee -a $LOG_FILE
-		exit 1
-	fi
-}
 
 dnf install mysql-server -y  &>> $LOG_FILE
 Validate $? "Installing mysql server module"
