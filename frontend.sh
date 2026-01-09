ab2d3faece687d675693f21f6092f08d028b870a
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-01-09 20:49:55 +0530

    roboshop

--- frontend.sh
+++ frontend.sh
@@ -1,33 +1,9 @@
 #! /bin/bash
 
-USERID=$(id -u)
-red="\e[31m"
-green="\e[32m"
-reset="\e[0m"
-LOGS_FOLDER="/var/logs/roboshop-log"
-SCRIPT_NAME=$(echo $0|cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+app_name=frontend
+source ./common.sh
+check_root
 
-mkdir -p ${LOGS_FOLDER}
-
-if [ $USERID -ne 0 ]
-then 
-	echo -e "${red}Error:${reset} Please run with root access...." | tee -a $LOG_FILE
-	exit 1
-else
-	echo -e "${green}Running with root access!!! ${reset}" | tee -a $LOG_FILE
-fi
-
-Validate() {
-	if [ $1 -eq 0 ]
-	then 
-		echo -e "${green}$2 is done successfully${reset}" | tee -a $LOG_FILE
-	else
-		echo -e "${red}Error: $2 failed${reset}" | tee -a $LOG_FILE
-		exit 1
-	fi
-}
 
 dnf module disable nginx -y &>> $LOG_FILE
 Validate $? "Disabiling nginx"
